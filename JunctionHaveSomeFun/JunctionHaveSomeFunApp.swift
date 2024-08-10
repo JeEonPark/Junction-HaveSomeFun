7b07b007023a03424fe768eb12f9f115f4d70e08
JeEon Park <ggungtoo@icloud.com>
2024-08-10 10:29:49 +0900

    first init

---
+++ JunctionHaveSomeFun/JunctionHaveSomeFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  JunctionHaveSomeFunApp.swift
+//  JunctionHaveSomeFun
+//
+//  Created by DevJonny on 2024/8/10.
+//
+
+import SwiftUI
+
+@main
+struct JunctionHaveSomeFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
