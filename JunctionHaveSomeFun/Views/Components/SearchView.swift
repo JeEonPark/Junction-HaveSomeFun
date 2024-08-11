32ef7526007b1709749a4136a62074eaa3ba89b1
Sean <dnxogns0101@naver.com>
2024-08-11 10:28:45 +0900

    popoverview

--- JunctionHaveSomeFun/Views/Components/SearchView.swift
+++ JunctionHaveSomeFun/Views/Components/SearchView.swift
@@ -8,16 +8,27 @@
 import SwiftUI
 
 struct SearchView: View {
+    @State var locationPopover = false
     @State var location = ""
     var body: some View {
         VStack(alignment: .leading, spacing: 0) {
-            HStack {
-                Image("bxs_map")
-                    .resizable()
-                    .frame(width: 32, height: 32)
-                
-                TextField("Select a location", text: $location)
-            }
+            
+            
+            //                TextField("Select a location", text: $location)
+            Button(action: {
+                locationPopover.toggle()
+            }, label: {
+                HStack {
+                    Image("bxs_map")
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                    if location == "" {
+                        Text("Select a location")
+                    } else {
+                        Text(location)
+                    }
+                }
+            })
         }
         .padding(.horizontal, 12)
         .padding(.vertical, 0)
@@ -26,10 +37,15 @@ struct SearchView: View {
         .cornerRadius(8)
         .shadow(color: .black.opacity(0.25), radius: 5, x: 4, y: 4)
         .overlay(
-          RoundedRectangle(cornerRadius: 8)
-            .inset(by: 0.75)
-            .stroke(Color(red: 0, green: 0.39, blue: 0.86), lineWidth: 1.5)
+            RoundedRectangle(cornerRadius: 8)
+                .inset(by: 0.75)
+                .stroke(Color(red: 0, green: 0.39, blue: 0.86), lineWidth: 1.5)
         )
+        .popover(isPresented: $locationPopover, attachmentAnchor: .point(.center), arrowEdge: .top) {
+            LocationSelectView(recommendPopover: $locationPopover, location: $location)
+                .frame(width: 244, height: 466)
+            
+        }
     }
 }
 
